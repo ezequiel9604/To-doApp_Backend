535f860704ff41b9ae609f8d2c62a54ff93a0279
Ezequiel Diaz Peña <ezequiel9604@gmail.com>
2022-07-27 22:02:31 +0800

    fix(WebApi): appsettings.json and appsettings.Development.json was modified in other to add configuration data need to the project like: database connection string, token keys and mail settings... feat(Infrastructure): a new folder called (Settings) was added, this includes a classes that will provide the configuration data needed to send mails... /

--- Infrastructure/Repositories/MailRepository.cs
+++ Infrastructure/Repositories/MailRepository.cs
@@ -1,17 +1,52 @@
 ﻿
 using MimeKit;
 using MailKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
 using Domain.Models;
 using Domain.Repositories;
+using Infrastructure.Settings;
+using Microsoft.Extensions.Options;
 
 namespace Infrastructure.Repositories;
 
 public class MailRepository : IMailRepository
 {
-    public async Task SendEmailAsync(MailRequest obj)
+
+    private readonly MailSettings _mailSettings;
+
+    public MailRepository(IOptions<MailSettings> mailSettings)
     {
-        
-        throw new NotImplementedException();
+        _mailSettings = mailSettings.Value;
+    }
+
+    public async Task SendEmailAsync(MailRequest mailRequest)
+    {
+
+        try
+        {
+            var message = new MimeMessage();
+            message.Sender = MailboxAddress.Parse(_mailSettings.UserName);
+            message.To.Add(MailboxAddress.Parse(mailRequest.ToEmail));
+            message.Subject = mailRequest.Subject;
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = mailRequest.Body;
+            message.Body = bodyBuilder.ToMessageBody();
+
+            var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.UserName, _mailSettings.Password);
+            await smtp.SendAsync(message);
+
+            smtp.Disconnect(true);
+            smtp.Dispose();
+
+        }
+        catch (Exception e)
+        {
+            throw new Exception(e.Message + "\n" + e.StackTrace);
+        }
 
     }
 }
---
+++ Infrastructure/Settings/MailSettings.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Infrastructure.Settings;
+
+public class MailSettings
+{
+
+    public string? UserName { get; set; }
+
+    public string? Password { get; set; }
+
+    public string? DisplayName { get; set; }
+
+    public string? Host { get; set; }
+
+    public int Port { get; set; }
+
+}
